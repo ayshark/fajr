4abec5be59269c2b65946fa04999112c111be534
ayshark <sjaysha@gmail.com>
2022-12-26 15:00:51 +0530

    Initial commit

---
+++ fajr/tracker/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from tracker.models import Tracker
+
+admin.site.register(Tracker)
---
+++ fajr/tracker/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+class Tracker(models.Model):
+    STATUS = (
+        ('Y', 'yes'),
+        ('N', 'no')
+    )
+    date = models.DateField(unique = True)
+    daily_status = models.CharField(max_length = 1, choices = STATUS)
+
+    def __str__(self):
+        return self.date, self.daily_status
+
