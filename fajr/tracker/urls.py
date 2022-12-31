f357e420b5e9114bb239961d8f5e212276116e37
ayshark <sjaysha@gmail.com>
2022-12-31 22:51:42 +0530

    updated urls

--- fajr/tracker/urls.py
+++ fajr/tracker/urls.py
@@ -12,7 +12,7 @@ urlpatterns = [
     path('delete/<int:pk>', views.delete_data, name = 'delete-record'),
 
     path('api/', include(router.urls)),
-    path('api/<int:pk>', include(router.urls))
+    # path('api/<int:pk>', include(router.urls))
     # path('', include(router.urls)),
     # path('api-auth/', include('rest_framework.urls', namespace = 'rest_framework')),
     # path('read/', views.read_data, name = 'view-records'),
