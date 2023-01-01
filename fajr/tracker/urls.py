c7dfba1124a3aff0a0816e64aa53510776e84155
ayshark <sjaysha@gmail.com>
2023-01-01 22:32:44 +0530

    APIViews endpoints

--- fajr/tracker/urls.py
+++ fajr/tracker/urls.py
@@ -2,8 +2,8 @@ from django.urls import include, path
 from rest_framework import routers
 from . import views
 
-router = routers.DefaultRouter()
-router.register(r'tracker', views.TrackerViewSet)
+# router = routers.DefaultRouter()
+# router.register(r'tracker', views.TrackerViewSet)
 
 urlpatterns = [
     path('', views.read_data, name = 'read-data'),
@@ -11,11 +11,15 @@ urlpatterns = [
     path('edit/<int:pk>', views.edit_data, name = 'edit-record'),
     path('delete/<int:pk>', views.delete_data, name = 'delete-record'),
 
-    path('api/', include(router.urls)),
+    # path('api/', include(router.urls)),
     # path('api/<int:pk>', include(router.urls))
+
     # path('', include(router.urls)),
     # path('api-auth/', include('rest_framework.urls', namespace = 'rest_framework')),
     # path('read/', views.read_data, name = 'view-records'),
+
+    path('api/', views.TrackerAPIView.as_view()),
+    path('api/<int:id>/', views.TrackerDetailAPIView.as_view())
 
     
 ]
--- fajr/tracker/views.py
+++ fajr/tracker/views.py
@@ -1,13 +1,111 @@
 from django.shortcuts import render, redirect
 
-from rest_framework import viewsets
+from rest_framework import viewsets, permissions, status
+from rest_framework.views import APIView
+from rest_framework.response import Response
 from .serializer import TrackerSerializer
 from .models import Tracker
 from .forms import TrackerForm
 
+
+
+ #creating API endpoints using APIView
+
+class TrackerAPIView(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    #list view
+    def get(self, request):
+        record = Tracker.objects.all().order_by('date')
+        serializer = TrackerSerializer(record, many = True)
+        return Response(
+            serializer.data,
+            status = status.HTTP_200_OK)
+        
+    
+    def post(self, request):
+        data = {
+            'date': request.data.get('date'),
+            'daily_status': request.data.get('daily_status')
+        }
+        serializer = TrackerSerializer(data = data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(
+                serializer.data, 
+                status = status.HTTP_201_CREATED
+            )
+        return Response(
+            serializer.errors,
+            status = status.HTTP_400_BAD_REQUEST
+        )
+
+class TrackerDetailAPIView(APIView):
+    permisssion_classes = [permissions.IsAuthenticated]
+
+    def get_record(self, id):
+        try:
+            return Tracker.objects.get(id = id)
+        except:
+            return None
+
+    def get(self, request, id):
+        record = self.get_record(id)
+        if not record:
+            return Response(
+                    {'res': 'Object does not exist'},
+                    status = status.HTTP_400_BAD_REQUEST
+            )
+        serializer = TrackerSerializer(record)
+        return Response(
+            serializer.data, 
+            status = status.HTTP_200_OK
+        )
+    
+    def delete(self, request, id):
+        record = self.get_record(id)
+        if not record:
+            return Response(
+                {'res': 'Object does not exist'},
+                status = status.HTTP_400_BAD_REQUEST
+            )
+        record.delete()
+        return Response(
+            {'res': 'Record deleted'},
+            status = status.HTTP_200_OK
+        )
+
+    def put(self, request, id):
+        record = self.get_record(id)
+        if not record:
+            return Response(
+                {'res': 'Object does not exist'},
+                status = status.HTTP_400_BAD_REQUEST
+            )
+        data = {
+            'date': request.data.get('date'),
+            'daily_status': request.data.get('daily_status')
+        }
+        serializer = TrackerSerializer(data = data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(
+                serializer.data,
+                status = status.HTTP_200_OK
+            )
+        return Response(
+            serializer.errors,
+            status = status.HTTP_400_BAD_REQUEST
+        )
+
+
+
+# UI view
+
 class TrackerViewSet(viewsets.ModelViewSet):
     queryset = Tracker.objects.all().order_by('date')
     serializer_class = TrackerSerializer
+    http_method_names = ['get', 'post', 'put', 'patch', 'delete']
 
 def read_data(request):
     records = Tracker.objects.all().order_by('date')
@@ -67,3 +165,5 @@ def delete_data(request, pk):
 
 
     #C:\\Users\Aysha\Desktop\\misc\django2\\fajr\\fajr\\tracker\\templates\\tracker\\read.html
+
+   
